f8dcd79578409ef134122161e63e9320c51bf1c8
Catchem87 <moras.ashley87@gmail.com>
2019-03-20 15:59:50 -0400

    heroku issues

--- config/connection.js
+++ config/connection.js
@@ -10,19 +10,12 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'hacktheplanet',
-        database: 'todoagain_db'
+        password: 'root',
+        database: 'burgers_db'
     });
 };
 
-// Make connection
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+
 
 // Export connection
 module.exports = connection;
